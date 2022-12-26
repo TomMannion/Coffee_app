136ce166defe2a605bbd4ba53ecd307c4c1d64a3
postie89 <t.mannion89@gmail.com>
2022-12-26 10:48:50 +0000

    import waitFor coffeeCard test

--- client/src/components/CoffeeCard.unit.test.js
+++ client/src/components/CoffeeCard.unit.test.js
@@ -1,4 +1,4 @@
-import { render, screen, act } from "@testing-library/react";
+import { render, screen, act, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CoffeeCard from "./CoffeeCard";
 // pass in props to CoffeeCard
