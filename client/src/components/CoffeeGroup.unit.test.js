0b8d5ad2e2bff85b3e1ea625aa145e47f8dc2bfe
postie89 <t.mannion89@gmail.com>
2023-01-01 13:42:19 +0000

    Added mock for CoffeeCard for unit testing CoffeeGroup

---
+++ client/src/components/CoffeeGroup.unit.test.js
@@ -0,0 +1,7 @@
+import { render, screen, act, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CoffeeGroup from "./CoffeeGroup";
+
+jest.mock("./CoffeeCard");
+
+describe("CoffeeGroup", () => {});
