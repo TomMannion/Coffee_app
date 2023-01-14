af6e617385ed3d42dac2a90d4f90a0df0010ab53
postie89 <t.mannion89@gmail.com>
2023-01-14 10:03:01 +0000

    added test for aeropress button in BrewMethod

--- client/src/components/BrewMethod.unit.test.js
+++ client/src/components/BrewMethod.unit.test.js
@@ -1,21 +1,33 @@
-import { screen, act } from "@testing-library/react";
+import { screen, act, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { renderWithRedux } from "../utils/utils-for-tests";
+import * as reactRedux from "react-redux";
 import BrewMethod from "./BrewMethod";
 
+jest.mock("react-redux", () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
+}));
+
+const mockStore = {
+  brewMethod: { brewMethod: { value: "" } },
+};
+
 describe("BrewMethod", () => {
-  const initialState = { brewMethod: { value: "" } };
-  test("has a brewMethod input", () => {
-    renderWithRedux(<BrewMethod />, { initialState });
-    const brewMethod = screen.getByLabelText("Brew Method");
-    expect(brewMethod).toBeInTheDocument();
+  beforeEach(() => {
+    useDispatchMock.mockImplementation(() => () => {});
+    useSelectorMock.mockImplementation((selector) => selector(mockStore));
   });
-  test("has a brewMethod input that can be typed into", async () => {
-    renderWithRedux(<BrewMethod />, { initialState });
-    const brewMethod = screen.getByLabelText("Brew Method");
-    act(() => {
-      userEvent.type(brewMethod, "French Press");
-    });
-    expect(brewMethod).toHaveValue("French Press");
+  afterEach(() => {
+    useDispatchMock.mockClear();
+    useSelectorMock.mockClear();
+  });
+
+  const useSelectorMock = reactRedux.useSelector;
+  const useDispatchMock = reactRedux.useDispatch;
+
+  test("test if Aeropress exists", () => {
+    render(<BrewMethod />);
+    const aeropress = screen.getByText("Aeropress");
+    expect(aeropress).toBeInTheDocument();
   });
 });
